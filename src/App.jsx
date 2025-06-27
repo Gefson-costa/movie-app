7e38037c495192beb45fbcbc8997ce32ef51eecc
Gefson Costa <gefson.code@proton.me>
2025-06-27 20:18:23 +0100

    building

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
 
-export default function App() {
+function App() {
 
   return (
-    <div>
-      <h1 class="bg-amber-400">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <img src="./hero.png" alt="Hero Banner"/>
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+
+    </main>
   )
 }
+
+
+export default App;
