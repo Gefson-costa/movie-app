21c7ec69eddb6c1c004dd47034f23da04d0523d8
Gefson Costa <gefson.code@proton.me>
2025-06-27 20:31:47 +0100

    building

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import Search from "./components/Search";
+
+
 
 
 function App() {
@@ -7,12 +10,12 @@ function App() {
       <div className="pattern" />
 
       <div className="wrapper">
-        <img src="./hero.png" alt="Hero Banner"/>
+        <img src="./hero.png" alt="Hero Banner" />
         <header>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
 
-        <p>Search</p>
+        <Search />
       </div>
 
     </main>
